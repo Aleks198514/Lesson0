272ca1ea34d96a180cd2fcafeb20ee8970b17b8e
Aleks198514 <Driller1985@mail.ru>
2024-08-10 06:24:12 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+print(12+89-1)
+
+print((9**0.5)*5) #1st program
+
+print(9.99>9.98)
+print(1000 != 1000.1)#2nd program
+
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2))) #3rd program
+
+print('123.456')
+print('123.456->123.456')
+print(123.456*10)
+print(int(1234.56))#4th program
